8da118e16873a45ad09175a53a504cfe9004686c
Steve Black <SteveBlackecmpe@gmail.com>
2021-02-14 22:21:58 -0500

    created class structure, and team files, created basic links

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// somthing 
+const inquirer = require("inquirer");
+const Team = require("./lib/Team");
+
+// this file starts the process...
+
+
+// class Employee {
+//     constructor(name, id, email){
+//         //add input validation
+//         this.name = name;
+//         this.id = id;
+//         this.email = email;
+//         this.emp = "Employee";
+//     }
+
+//     getName(){
+
+//     };
+
+//     getId(){
+
+//     };
+
+//     getEmail(){
+
+//     };
+
+//     getRole(emp){
+//         return emp.role;
+//     }
+// }
+
