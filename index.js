c3fa40c0658185c3b192624424c483a8d39224ed
Steve Black <SteveBlackecmpe@gmail.com>
2021-02-17 18:37:23 -0500

    updated tests and building out engineer

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 // const Team = require("./lib/Team");
 const GenHTML = require("./lib/generateHTML");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -12,15 +15,28 @@ const teamMembers = [];
 
 function addTeamMember(data) {
     teamMembers.push(data);
-    console.log(teamMembers);
+    // console.log(teamMembers);
 }
 
 function checkTeamFor(role) {
     let teamArrLength = teamMembers.length;
     for (let i = 0; i < teamArrLength; i++) {
-        if (teamMembers[i].role === role) {
+        if (teamMembers[i].getRole() === role) {
             return true;
         } else {
+            return false;
+        }
+    }
+}
+
+function checkTeamIdFor(id) {
+    let teamArrLength = teamMembers.length;
+    for (let i = 0; i < teamArrLength; i++) {
+        if (teamMembers[i].getId() === id) {
+            return true;
+        } else {
+            console.log(teamMembers[i].getId());
+            console.log(id);
             return false;
         }
     }
@@ -58,9 +74,24 @@ const ManagerQuestions = [
 const EngineerQuestions = [
     {
         type: 'input',
-        name: 'projectTitle',
-        message: 'Who is the team manager?',
-    }
+        name: 'name',
+        message: 'What is the Engineers Name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the Engineers ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the Engineers email?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is the Engineers GitHub User Name?',
+    },
 ];
 
 const InternQuestions = [
@@ -71,7 +102,6 @@ const InternQuestions = [
     }
 ];
 
-
 const MenuQuestions = [
     {
         type: 'list',
@@ -81,33 +111,40 @@ const MenuQuestions = [
     }
 ];
 
-function getManager() {
+function getManager(id) {
     if (checkTeamFor("Manager")) {
         console.log("This team already has a Manager");
+        init();
+    } else if (checkTeamIdFor(id)) {
+        console.log("This id is already in use");
         init();
     } else {
         inquirer
             .prompt(ManagerQuestions)
             .then((data) => {
-                console.log(data);
-                // build new manager
-                data.role = "Manager";
-                addTeamMember(data);
+                // console.log(data);
+                let manager = new Manager(data.ManagerName, data.ManagerID, data.ManagerEmail, data.ManagerOfficeNumber);
+                addTeamMember(manager);
                 init();
             })
     }
 }
 
-function getEngineer() {
-    inquirer
-        .prompt(EngineerQuestions)
-        .then((data) => {
-            // console.log(data);
-            // build new engineer
-            data.role = "Engineer";
-            addTeamMember(data);
-            init();
-        })
+function getEngineer(id) {
+    console.log("GetEng " + id);
+    if (checkTeamIdFor(id)) {
+        console.log("This id is already in use");
+        init();
+    } else {
+        inquirer
+            .prompt(EngineerQuestions)
+            .then((data) => {
+                // console.log(data);
+                let engineer = new Engineer(data.name, data.id, data.email, data.github);
+                addTeamMember(engineer);
+                init();
+            })
+    }
 }
 
 function getIntern() {
@@ -137,10 +174,11 @@ function init() {
             switch (data.choice) {
                 case 'Add Manager':
                     console.log("Yeah add a manager");
-                    getManager();
+                    getManager(data.id);
                     break;
                 case 'Add Engineer':
-                    console.log("Yeah add a engineer");
+                    console.log("Yeah add a engineer " + data.id );
+                    getEngineer(data.id);
                     break;
                 case 'Add intern':
                     console.log("Yeah add a intern");
